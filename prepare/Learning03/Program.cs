a421bb43585cde0fa62f03bfc6c8ffdf620ec1a2
iabiloye <iabiloye@gmail.com>
2023-01-13 00:38:41 +0100

    week2

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+  
+        Random randomGenerator = new Random();
+        int x = randomGenerator.Next(1, 100);
+        
+        
+        int guessNumber = 0;        
+
+        while (guessNumber != x)
+        {
+            Console.Write("What is your guess? ");
+            string userInputGuess = Console.ReadLine();
+            guessNumber = int.Parse(userInputGuess);
+            
+                if (guessNumber < x)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guessNumber > x)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it! You must have ESPN!(joke) ");
+            }
+        }
+
     }
 }
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        string userInput = "";
+
+ 
+        List<int> numbers = new List<int>();
+
+       
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        while (userInput != "0")
+        {
+            
+            Console.Write("Enter number: ");
+            userInput = Console.ReadLine();
+            int userNumber = int.Parse(userInput);
+
+            if (userNumber !=0)
+            {
+                numbers.Add(userNumber);
+            }
+            
+        }
+       
+        float total = numbers.Sum();
+        Console.WriteLine($"The sum is: {total}");
+        
+        float count = numbers.Count();
+        float average = total / count;
+        Console.WriteLine($"The average is: {average}");
+
+        
+        int max = numbers.Max();
+        Console.WriteLine($"The largest number is: {max}");
+
+        
+
+        
+        
     }
 }
